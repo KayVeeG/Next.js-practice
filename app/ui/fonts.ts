7e798bbcc3970a5a73032d73128a9ea54e797b8e
KayVeeG <Karl.vongagern@gmail.com>
2025-05-13 09:58:18 +0200

    fix fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { Inter } from 'next/font/google';
 import { Roboto } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const roboto = Roboto({ weight: ['500']});
+export const roboto = Roboto({ subsets: ['latin'], weight: ['500']});
